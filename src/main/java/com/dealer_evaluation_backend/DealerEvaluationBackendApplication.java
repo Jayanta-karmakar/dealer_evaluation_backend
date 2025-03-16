fa58d4be9708d677abddfc080f6e4eb09dc8d84a
Jayanta Karmakar <jayantakarmakar998@gmail.com>
2025-03-16 14:29:45 +0530

    initial commit with project structure

---
+++ src/main/java/com/dealer_evaluation_backend/DealerEvaluationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dealer_evaluation_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealerEvaluationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealerEvaluationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dealer_evaluation_backend/DealerEvaluationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dealer_evaluation_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealerEvaluationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
